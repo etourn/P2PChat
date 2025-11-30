162fe28053a595ebfcaeed8da71f81a9aae2cff7
Elene Sturua <sturuael@grinnell.edu>
2025-11-30 16:48:14 -0600

    we were sending to ourselves instead of blocking ourselves as recievers, so I just reversed the logic by sending to peers[i]

--- p2pchat.c
+++ p2pchat.c
@@ -11,7 +11,7 @@
 #include "reading.h"
 
 // Keep the username in a global so we can access it from the callback
-const char* username;
+const char *username;
 
 #define CAPACITY 1000
 #define MESSAGE_LEN 2048
@@ -19,52 +19,67 @@ pthread_mutex_t peers_lock = PTHREAD_MUTEX_INITIALIZER;
 
 // List of peers
 intptr_t peers[CAPACITY];
-int num_peers=0;
+int num_peers = 0;
 
 // Broadcast message
-void broadcast(int unique_fd, char* username, char* message){
+void broadcast(int unique_fd, char *username, char *message)
+{
 
   // get lengths of our data
   size_t ulen = strlen(username);
   size_t mlen = strlen(message);
 
   // Write to all peers
-  for (int i = 0; i < num_peers; i++) {    
-    if (write_helper(unique_fd, (char*) &ulen, sizeof(size_t)) != (ssize_t)sizeof(size_t)) {
-      break;
+  for (int i = 0; i < num_peers; i++)
+  {
+    if (peers[i] == unique_fd)
+      continue;
+
+    if (write_helper(peers[i], (char *)&ulen, sizeof(size_t)) != (ssize_t)sizeof(size_t))
+    {
+      continue;
       fprintf(stderr, "Error transmitting over network\n");
     }
-    if (write_helper(unique_fd, username, ulen) != (ssize_t)ulen) {
-      break;
+    if (write_helper(peers[i], username, ulen) != (ssize_t)ulen)
+    {
+      continue;
       fprintf(stderr, "Error transmitting over network\n");
     }
-    if (write_helper(unique_fd, (char*) &mlen, sizeof(size_t)) != (ssize_t)sizeof(size_t)) {
-      break;
+    if (write_helper(peers[i], (char *)&mlen, sizeof(size_t)) != (ssize_t)sizeof(size_t))
+    {
+      continue;
       fprintf(stderr, "Error transmitting over network\n");
     }
-    if (write_helper(unique_fd, message, mlen) != (ssize_t)mlen) {
-      break;
+    if (write_helper(peers[i], message, mlen) != (ssize_t)mlen)
+    {
+      continue;
       fprintf(stderr, "Error transmitting over network\n");
     }
   }
 }
 
 // Thread for accepting incoming connection thread
-void* accept_thread(void* arg) {
-  intptr_t server_fd = (intptr_t) arg;
+void *accept_thread(void *arg)
+{
+  intptr_t server_fd = (intptr_t)arg;
   // Continuously accepting thread as long as the server is running
-  while(1) {
-    // Get the socket file descriptor 
+  while (1)
+  {
+    // Get the socket file descriptor
     intptr_t peer_fd = server_socket_accept(server_fd);
     // Skip if accept return an error
-    if (peer_fd < 0) continue;
-    
+    if (peer_fd < 0)
+      continue;
+
     // Add new peers to the global peer list
     pthread_mutex_lock(&peers_lock);
-    if (num_peers < CAPACITY) {
+    if (num_peers < CAPACITY)
+    {
       // store peers
       peers[num_peers++] = peer_fd;
-    } else {
+    }
+    else
+    {
       // too many peers
       close(peer_fd);
       pthread_mutex_unlock(&peers_lock);
@@ -74,24 +89,30 @@ void* accept_thread(void* arg) {
 
     // Create a read thread for each peer
     pthread_t t;
-    pthread_create(&t, NULL, peer_read_thread, (void*) peer_fd);
+    pthread_create(&t, NULL, peer_read_thread, (void *)peer_fd);
   }
   return NULL;
 }
 
 // This function is run whenever the user hits enter after typing a message
-void input_callback(const char* message) {
-  if (strcmp(message, ":quit") == 0 || strcmp(message, ":q") == 0) {
+void input_callback(const char *message)
+{
+  if (strcmp(message, ":quit") == 0 || strcmp(message, ":q") == 0)
+  {
     ui_exit();
-  } else {
+  }
+  else
+  {
     ui_display(username, message);
-  } 
+  }
   // TODO: construct a message and send it to everyone else
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   // Make sure the arguments include a username
-  if (argc != 2 && argc != 4) {
+  if (argc != 2 && argc != 4)
+  {
     fprintf(stderr, "Usage: %s <username> [<peer> <port number>]\n", argv[0]);
     exit(1);
   }
@@ -102,23 +123,26 @@ int main(int argc, char** argv) {
   // TODO: Set up a server socket to accept incoming connections
   unsigned short port = 0;
   intptr_t server_socket_fd = server_socket_open(&port);
-  if (server_socket_fd == -1) {
+  if (server_socket_fd == -1)
+  {
     perror("Server socket was not opened");
     exit(EXIT_FAILURE);
   }
 
   // listening on our server
   pthread_t thread_id;
-  pthread_create(&thread_id, NULL, accept_thread, (void*) server_socket_fd);
+  pthread_create(&thread_id, NULL, accept_thread, (void *)server_socket_fd);
 
   // Did the user specify a peer we should connect to?
-  if (argc == 4) {
+  if (argc == 4)
+  {
     // Unpack arguments
-    char* peer_hostname = argv[2];
+    char *peer_hostname = argv[2];
     unsigned short peer_port = atoi(argv[3]);
 
     // TODO: Connect to another peer in the chat network
-    if (socket_connect(peer_hostname, peer_port) == -1) {
+    if (socket_connect(peer_hostname, peer_port) == -1)
+    {
       perror("Connection fail");
       exit(EXIT_FAILURE);
     }
